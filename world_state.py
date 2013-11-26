9396d25af1077bcd339d8df1165490cd65f243c3
elstupido <martinjg@gmail.com>
2013-11-26 11:55:54 -0500

    initial commit

---
+++ world_state.py
@@ -0,0 +1,17 @@
+class World(object):
+    
+    #world states
+    LOADING = 0
+    EXPLORING = 1
+    DIALOG = 2
+    FIGHT = 3
+    
+    
+    def __init__(self):
+        self.rooms = {}
+        self.characters = {}
+        self.items = {}    
+        self.current_state = self.LOADING
+        self.state = [self.current_state]
+    
+    
