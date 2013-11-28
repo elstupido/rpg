b64bb003548f85e656b73b077df801f3360fd332
shifty68 <vincentdmancino@hotmail.com>
2013-11-28 10:49:29 -0500

    new room

---
+++ rooms/prolog/window.room.py
@@ -0,0 +1,13 @@
+
+from room import Room
+r = Room()
+r.roomname = 'window'
+r.exits = 
+ 'floor 1': 'lobby',
+ 'floor 2': 'lounge',
+ 'hallway': 'hallway'}
+r.roomdesc = """
+falling (very windy)
+
+"""
+r.looktargets = 
